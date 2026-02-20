dcdc931a8349a5760816cd3ac22bf7e43c75b387
Megan WebDev <megan.tegeler@student.swic.edu>
2026-02-19 20:04:57 -0600

    Add personal information variables

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello via Bun (plain JS)!");
+// Personal Information
+const firstName = "Megan";
+const lastName = "Tegeler";
+const age = 30; // Your actual age
+const isStudent = true;
+
+// Using template literals
+const introduction = `Hello! My name is $Megan $Tegeler. I am ${age} years old.`;
+const studentStatus = `Am I a student? ${isStudent}`;
+
+// Print to console
+console.log(introduction);
+console.log(studentStatus);
